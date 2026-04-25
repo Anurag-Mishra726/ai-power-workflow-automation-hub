4d5944336599de3ff7a71ec0b22e6a69e2cb3bd4
Anurag-Mishra726 <anurag1192044@gmail.com>
2026-04-25 11:38:09 +0530

    refactor: update button elements in HeroSection and Navbar for consistency

--- frontend/src/components/landing/Navbar.jsx
+++ frontend/src/components/landing/Navbar.jsx
@@ -13,15 +13,15 @@ const Navbar = () => {
       </div>
       <div className="nav-center ">
         <ul className=" nav-elements flex items-center justify-between gap-[40px] ">
-          <li><Link to="/" className="nav-link">Home</Link></li>
-          <li><Link to="/features" className="nav-link">Features</Link></li>
-          <li><Link to="/integrations" className="nav-link">Integrations</Link></li>
-          <li><Link to="/developer" className="nav-link">Developer</Link></li>
-          <li><Link to="/about" className="nav-link">About</Link></li>
+          <li><Link to="/home" className="nav-link">Home</Link></li>
+          <li><Link to="#" className="nav-link">Features</Link></li>
+          <li><Link to="#" className="nav-link">Integrations</Link></li>
+          <li><Link to="#" className="nav-link">Developer</Link></li>
+          <li><Link to="#" className="nav-link">About</Link></li>
         </ul>
       </div>
       <div className="nav-right">
-        <Button text="Get Started" to="/auth/login" />
+        <button className='get-started-btn '><Button text="Get Started" to="/auth/login" /> </button>
       </div>
     </div>
   )
